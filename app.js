c8f3a8fb220a83127fd96487de54554700ec7127
bttf <adnanchowdhury88@gmail.com>
2014-05-30 21:44:27 -0400

    tidy things up

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 var fs = require('fs');
 var express = require('express');
 var app = express();
-var path = 'temp.wav';
+var engines = require('consolidate');
+var path = require('path');
+var wav = 'temp.wav';
+
+app.engine('html', engines.handlebars);
+app.set('view engine', 'html');
+app.set('views', path.join(__dirname, '/views'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res) {
+  res.render('index');
+});
+
+app.get('/elmo', function(req, res) {
+  // don't ask questions
+  res.render('elmo');
+});
 
 app.post('/audio', function(req, res, next) {
   if (req.get('Content-Type') !== 'audio/x-wav') {
@@ -9,7 +25,7 @@ app.post('/audio', function(req, res, next) {
   }
 
   console.log('opening stream for ' + path);
-  var stream = fs.createWriteStream(path);
+  var stream = fs.createWriteStream(wav);
   req.pipe(stream);
   req.on('end', next);
 });
